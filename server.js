5cb31fda1c078f59598be4b23f0fd445cfcfe8e8
Andrew Popp <apopp524@gmail.com>
2020-10-07 15:30:38 -0500

    deploying

--- server.js
+++ server.js
@@ -11,13 +11,19 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/fitness-tracker',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+  
 
 app.use(routes);
 
 app.listen(PORT, function () {
-    console.log("App running on port ${PORT}!")
+    console.log("App running on port 8000")
 }); 
