2322ffd5bc02439592245c17da980c0188c6d303
Andrew Popp <apopp524@gmail.com>
2020-10-09 14:57:48 -0500

    bug fixes and fixed the Total Duration issue

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,17 +1,12 @@
 const path = require("path");
 
-module.exports = function (app) {
-
-    app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
-
-    app.get("/exercise", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/exercise.html"));
-    });
-
-    app.get("/stats", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/stats.html"));
-    });
-
-}; 
+module.exports = function(app) {
+  // Called when "Countinue Workout" or "new Workout" is clicked in index.html
+  app.get("/exercise", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+  // Not quite sure what this is used for yet ....
+  app.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+};
