5c58fe1b5faa5c287be9f4198cd8081cd3fe7800
Andrew Popp <apopp524@gmail.com>
2020-10-09 13:11:54 -0500

    updated workout.js & added POST to mainAPI.js

--- routes/mainAPI.js
+++ routes/mainAPI.js
@@ -1,7 +1,8 @@
-const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
-router.get("/api/workout", (req, res) => {
+module.exports = function (app) {
+
+  app.get("/api/workouts", (req, res) => {
     Workout.find({})
       .then(dbWorkout => {
         res.json(dbWorkout);
@@ -11,4 +12,33 @@ router.get("/api/workout", (req, res) => {
       });
   });
 
-  module.exports = router;
+  app.post('/api/workouts', (req, res) => {
+    Workout.create({})
+        .then(dbWorkout => {
+          res.json(dbWorkout)
+        })
+        .catch((err) => {
+            res.status(400).json(err)
+        })
+  });
+
+  app.put('/api/workouts/:id', (req, res) => {
+    Workout.findByIdAndUpdate(req.params.id, {$push: {exercises: req.body}})
+        .then(dbWorkout => {
+          res.json(dbWorkout)
+        })
+        .catch((err) => {
+            res.status(400).json(err)
+        })
+  });
+
+  app.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+};
